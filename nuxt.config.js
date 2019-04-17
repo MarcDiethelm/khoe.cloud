a620df4d6fb02b1fe4f64927a10c6a3b60a9f65a
Marc Diethelm <mail@marcd.ch>
2019-04-17 12:15:42 +0200

    whitespace

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,68 +1,69 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: pkg.name,
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: pkg.description }
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
 
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
+	},
 
-  /*
-   ** Global CSS
-   */
-  css: [],
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: '#fff' },
 
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
+	/*
+	 ** Global CSS
+	 */
+	css: [],
 
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa'
-  ],
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [],
 
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		'@nuxtjs/pwa'
+	],
+	/*
+	 ** Axios module configuration
+	 */
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
+	},
+
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {
+			// Run ESLint on save
+			if (ctx.isDev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	}
 }
--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -5,8 +5,8 @@ const Vue = require('vue')
 Vue.config.productionTip = false
 
 hooks('vue')
-  .plugin('vue')
-  .push()
+	.plugin('vue')
+	.push()
 hooks(['vue', 'js'])
-  .plugin('babel')
-  .push()
+	.plugin('babel')
+	.push()
