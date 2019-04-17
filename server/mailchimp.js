3fd3b6141e1ca24cb6dbc141327f2b5076a6c69e
Marc Diethelm <mail@marcd.ch>
2019-04-17 16:27:33 +0200

    Prefix all env vars with `KHOE_`

--- server/mailchimp.js
+++ server/mailchimp.js
@@ -1,17 +1,17 @@
 
 const assert = require('assert')
-assert.ok(process.env.MAILCHIMP_API_KEY)
-assert.ok(process.env.MAILCHIMP_LIST_ID)
+assert.ok(process.env.KHOE_MAILCHIMP_API_KEY)
+assert.ok(process.env.KHOE_MAILCHIMP_LIST_ID)
 
 const Mailchimp = require('mailchimp-api-v3')
-const mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY)
+const mailchimp = new Mailchimp(process.env.KHOE_MAILCHIMP_API_KEY)
 
 function memberSubscribe(email, ip) {
 	return new Promise(function(resolve, reject) {
 		console.log(`creating mailchimp subscriber: ${email}, ip: ${ip}`)
 
 		mailchimp
-			.post(`/lists/${process.env.MAILCHIMP_LIST_ID}/members`, {
+			.post(`/lists/${process.env.KHOE_MAILCHIMP_LIST_ID}/members`, {
 				email_address: email,
 				ip_opt: ip, // Save user IP to Mailchimp opt-in IP field. (correct since were not using double opt-in.)
 				status: 'subscribed'
