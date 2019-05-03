b3d9eea3ea37f02a84dc51c9d1725939d7ad1f74
Marc Diethelm <mail@marcd.ch>
2019-05-04 00:41:41 +0200

    Disable unused UI framework resources

--- plugins/blackDashboard.js
+++ plugins/blackDashboard.js
@@ -1,9 +1,10 @@
-import SideBar from '@/components/dashboard/SidebarPlugin'
-import Notify from '@/components/dashboard/NotificationPlugin'
+// import SideBar from '@/components/dashboard/SidebarPlugin'
+// import Notify from '@/components/dashboard/NotificationPlugin'
 import GlobalComponents from './globalComponents'
-import GlobalDirectives from './globalDirectives'
-import RTLPlugin from './RTLPlugin'
+// import GlobalDirectives from './globalDirectives'
+// import RTLPlugin from './RTLPlugin'
 
+// moved to head...
 // css assets
 // import '@/assets/dashboard/sass/black-dashboard.scss'
 // import '@/assets/dashboard/css/nucleo-icons.css'
@@ -12,9 +13,9 @@ import RTLPlugin from './RTLPlugin'
 export default {
 	install(Vue) {
 		Vue.use(GlobalComponents)
-		Vue.use(GlobalDirectives)
-		Vue.use(SideBar)
-		Vue.use(Notify)
-		Vue.use(RTLPlugin)
+		// Vue.use(GlobalDirectives)
+		// Vue.use(SideBar)
+		// Vue.use(Notify)
+		// Vue.use(RTLPlugin)
 	}
 }
